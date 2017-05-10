f6762558e08f18c55cd3f6fb76ba3264c543fd94
Matt Wu <mattwu@zipcoder19.local>
2017-05-10 17:12:43 -0400

    lastOne to go

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -3,24 +3,125 @@ package io.zipcoder.microlabs.arrays;
 
 public class ArrayParty {
 
+
     public String printArray(String[] inputArray){
-        return "";
+
+        String result = "*** Output ***";
+
+        for (int i = 0; i < inputArray.length; i++){
+
+
+            result = result + "\n" + inputArray[i];
+        }
+
+        return result;
     }
+
 
     public String lastElement(String[] inputArray){
-        return "";
+
+        String result = "*** Output ***\n";
+
+        int lastOne = inputArray.length - 1;
+
+        return result + inputArray[lastOne];
     }
 
-    //TODO Define the method lastButOne
 
-    //TODO Define the method reverse
+    public String lastSecondElement(String[] inputArray){
 
-    //TODO Define the method isPalindrome
+        String result = "*** Output ***\n";
 
-    //TODO Define the method compress
+        int lastSecond = inputArray.length - 2;
+
+        return result + inputArray[lastSecond];
+
+    }
+
+
+    public String reverse(String[] inputArray){
+
+        String result = "*** Output ***";
+
+
+        for (int i = inputArray.length - 1 ; i >= 0; i --){
+
+            result = result + "\n" + inputArray[i];
+
+        }
+
+        return result;
+
+    }
+
+
+    public String isPalindrome(String[] inputArray){
+
+        String intro = "*** Output ***\n";
+
+        String result = "True";
+
+        int l = inputArray.length;
+
+        for (int i = 0; i < l; i++){
+
+            if (inputArray[i] != inputArray[l - (i + 1)]){
+
+                result = "False";
+                break;
+            }
+
+        }
+
+        return intro + result;
+
+    }
+
+
+    public String compress(int[] inputArray){
+
+        String result = "*** Output ***";
+
+        int temp = 0;
+
+        for (int i = 0; i < inputArray.length - 2; i++){
+
+            if (inputArray[i] == inputArray[i+1]){
+
+                int flag = inputArray[i];
+
+                if (flag != temp) {
+
+                    temp = flag;
+                    result = result + "\n: " + flag;
+                }
+            }
+
+        }
+
+        return result;
+
+    }
+
+
+
+
+
+
+
+
+
 
     //TODO Define the method pack
 
+    public static void main(String[] args) {
 
+        ArrayParty test = new ArrayParty();
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+        System.out.println(test.reverse(breakfast));
+
+
+
+    }
 
 }
--- src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
+++ src/test/java/io/zipcoder/microlabs/arrays/ArrayPartyTest.java
@@ -1,15 +1,22 @@
 package io.zipcoder.microlabs.arrays;
 
-import org.junit.Assert;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
+
 
 public class ArrayPartyTest {
+
+    ArrayParty arrayParty;
+    @Before
+    public void setUp(){ arrayParty = new ArrayParty(); }
 
     @Test
     public void printArrayTest(){
         //: Given
         String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
-        ArrayParty arrayParty = new ArrayParty();
+
         String expected = "*** Output ***\n" +
                 "Sausage\n" +
                 "Eggs\n" +
@@ -22,23 +29,104 @@ public class ArrayPartyTest {
         String actual = arrayParty.printArray(breakfast);
 
         //: Then
-        Assert.assertEquals("The two string outputs are equal", expected, actual);
+        assertEquals(expected, actual);
     }
+
 
     @Test
     public void lastElementTest(){
         //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+
+        String expected = "*** Output ***\nMushrooms";
 
         //: When
+        String actual = arrayParty.lastElement(breakfast);
 
         //: Then
+        assertEquals( expected, actual);
+
     }
 
-    //TODO Define the method lastButOneTest
 
-    //TODO Define the method reverseTest
+    @Test
+    public void lastSecondElementTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
 
-    //TODO Define the method isPalindromeTest
+        String expected = "*** Output ***\nTomatoes";
+
+        //: When
+        String actual = arrayParty.lastSecondElement(breakfast);
+
+        //: Then
+        assertEquals( expected, actual);
+
+    }
+
+    @Test
+    public void reverseTest(){
+        //: Given
+        String[] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+
+        String expected = "*** Output ***\nMushrooms\nTomatoes\nBacon\nBeans\nEggs\nSausage";
+
+        //: When
+        String actual = arrayParty.reverse(breakfast);
+
+        //: Then
+        assertEquals( expected, actual);
+
+    }
+
+    @Test
+    public void trueIsPalindromeTest(){  // test a true Palin to see if it returns true
+        //: Given
+        String [] breakfast = {"Sausage", "Eggs", "Beans", "Beans", "Eggs", "Sausage"};
+
+        String expected = "*** Output ***\nTrue";
+
+        //: When
+        String actual = arrayParty.isPalindrome(breakfast);
+
+        //: Then
+        assertEquals( expected, actual);
+
+    }
+
+    @Test
+    public void falseIsPalindromeTest(){  // test a true Palin to see if it returns false
+        //: Given
+        String [] breakfast = {"Sausage", "Eggs", "Beans", "Bacon", "Tomatoes", "Mushrooms"};
+
+        String expected = "*** Output ***\nFalse";
+
+        //: When
+        String actual = arrayParty.isPalindrome(breakfast);
+
+        //: Then
+        assertEquals( expected, actual);
+
+    }
+
+    @Test
+    public void compressTest(){  // test a true Palin to see if it returns false
+        //: Given
+        int [] nums = {1,1,3,3,3,2,2,2,1,1,1,1,4,4,4,4};
+
+        String expected = "*** Output ***\n: 1\n: 3\n: 2\n: 1\n: 4";
+
+        //: When
+        String actual = arrayParty.compress(nums);
+
+        //: Then
+        assertEquals( expected, actual);
+
+    }
+
+
+
+
 
     //TODO Define the method compressTest
 
